1d970b2d35480074a25110931fb2193c266c15e6
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-06-10 23:44:35 -0400

    Create LongestCommonPrefix.py

---
+++ AMZ/Easy/LongestCommonPrefix.py
@@ -0,0 +1,25 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+"""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans = ""
+        strs = sorted(strs) # handling strs = ["aaa","aa","aaa"]
+        first = strs[0]
+        last = strs[-1]
+        for i in range(min(len(first), len(last))):
+            print(first[i], last[i], ans)
+            if(first[i] != last[i]):
+                return ans
+            ans += first[i]
+        return ans 
