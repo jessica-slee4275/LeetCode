dc21ff48426b7e766c8bda90f325bb5e8e3da9ae
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-06-13 21:39:43 -0400

    Create LetterCombinationsofaPhoneNumber.py

---
+++ AMZ/Medium/LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,55 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+phone = {
+            '2': ["a","b","c"],
+            '3': ["d","e","f"],
+            '4': ["g","h","i"],
+            '5': ["j","k","l"],
+            '6': ["m","n","o"],
+            '7': ["p","q","r", "s"],
+            '8': ["t","u","v"],
+            '9': ["w","x","y","z"]
+        }
+
+Example 1:
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Example 2:
+Input: digits = ""
+Output: []
+
+Example 3:
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+Constraints:
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+"""
+import itertools
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {
+            '2': ["a","b","c"],
+            '3': ["d","e","f"],
+            '4': ["g","h","i"],
+            '5': ["j","k","l"],
+            '6': ["m","n","o"],
+            '7': ["p","q","r", "s"],
+            '8': ["t","u","v"],
+            '9': ["w","x","y","z"]
+        }
+        if len(digits) == 0: return []
+        temp = phone[digits[0]]
+
+        for i in range(1, len(digits)):
+            arr = []
+            for p in list(itertools.product(phone[digits[i]], temp)):
+                arr.append(''.join(list(p)))
+            temp = arr
+        res = []
+        for t in temp:
+            res.append(t[::-1])
+        return res
