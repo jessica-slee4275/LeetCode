8f434ac9056398ee3966ead1b173f98c6b29edf5
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-06-18 08:22:23 -0400

    Update FindtheIndexoftheFirstOccurrenceinaString.py

--- AMZ/Easy/FindtheIndexoftheFirstOccurrenceinaString.py
+++ AMZ/Easy/FindtheIndexoftheFirstOccurrenceinaString.py
@@ -13,7 +13,7 @@ Output: -1
 Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 Constraints:
-1 <= haystack.length, needle.length <= 104
+1 <= haystack.length, needle.length <= 10**4
 haystack and needle consist of only lowercase English characters.
 """
 class Solution:
