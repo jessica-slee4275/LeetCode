360ecab59f71873680c76f9dbf4ce473298e900e
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-07-29 23:43:37 -0400

    Create NumberOfIslands.py

---
+++ GGL/Medium/NumberOfIslands.py
@@ -0,0 +1,47 @@
+"""
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+
+Example 2:
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'.
+"""
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        cnt = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    cnt += 1
+        return cnt
+
+    def dfs(self, grid, i, j):
+        grid[i][j] = 0
+        for a, b in (1, 0), (-1, 0), (0, -1), (0, 1):
+            # print(a, b, i, j)
+            a += i
+            b += j
+            if 0 <= a < len(grid) and 0 <= b < len(grid[0]) and grid[a][b]=='1':
+                self.dfs(grid, a, b)
