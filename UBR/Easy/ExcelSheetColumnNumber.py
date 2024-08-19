8bdae0e97f8e9a83ac7ab8f901d84d68035232aa
jessica-slee4275 <50062122+jessica-slee4275@users.noreply.github.com>
2024-08-19 19:46:47 -0400

    Create ExcelSheetColumnNumber.py

---
+++ UBR/Easy/ExcelSheetColumnNumber.py
@@ -0,0 +1,39 @@
+"""
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+Example 1:
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+Input: columnTitle = "ZY"
+Output: 701
+ 
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+"""
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        power = 1
+        for char in columnTitle[::-1]:
+            result += (ord(char) - ord('A') + 1) * power
+            power *= 26
+        return result
